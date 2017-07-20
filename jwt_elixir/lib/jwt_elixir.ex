67604af7842b6f56f77129433bf112c734c9a955
kepung <kepung@gmail.com>
2017-07-20 18:00:54 +1200

    jwt initial project check in.

---
+++ jwt_elixir/lib/jwt_elixir.ex
@@ -0,0 +1,18 @@
+defmodule JwtElixir do
+  @moduledoc """
+  Documentation for JwtElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> JwtElixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ jwt_elixir/test/jwt_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule JwtElixirTest do
+  use ExUnit.Case
+  doctest JwtElixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
